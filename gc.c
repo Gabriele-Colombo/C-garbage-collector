24a84d2597cc0b07fa7e752b83f08183893a3bd2
Gabriele Colombo <colombogabriele0303@gmail.com>
2025-08-14 23:52:59 +0200

    Added sweep and easy printing

--- gc.c
+++ gc.c
@@ -1,14 +1,21 @@
-#define STACK_MAX 256
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
 
-// Create objects type in the VM
+#define STACK_MAX 256
+#define INITIAL_THRESHOLD 5
+
+
 typedef enum{
     INT,
     DOUBLE,
     PAIR
 }ObjectType;
 
-// Create objects
+
 typedef struct fObject{
+
+    struct fObject* next;
 
     // Mark
     unsigned char marked;
@@ -22,71 +29,118 @@ typedef struct fObject{
         /* Value for DOUBLE*/
         double doubleValue;
         /* Value for PAIR_INT*/
-        typedef struct{
+        struct{
             struct fObject* head;
-            struct fObject* tail
+            struct fObject* tail;
         };
     };
 }Object;
 
+
 // Create a VM as an array of Object
 typedef struct{
-    Object* stack[STACK_MAX]
+
+    int numObject; // total allocated Object
+    int maxObject; // Max number of object to activate the gc
+
+    Object* firstObject; // Objects created
+
+    Object* stack[STACK_MAX]; // Object in stack
     int stackSize;
 }VM;
 
+
+VM* newVM();
+void push(VM* vm, Object* obj);
+Object* pop(VM* vm);
+void gc(VM* vm);
+Object* createObject(VM* vm, ObjectType objType);
+void pushInt(VM* vm, int value);
+void pushDouble(VM* vm, double value);
+void pushPair(VM* vm);
+void mark(Object* obj);
+void sweep(VM* vm);
+void printStack(VM* vm);
+void printObjects(VM* vm);
+void printObject(Object* obj);
+
+
 VM* newVM(){
     VM* vm = malloc(sizeof(VM));
+    vm->firstObject = NULL;
     vm->stackSize = 0;
+    vm->numObject = 0;
+    vm->maxObject = INITIAL_THRESHOLD;
+
+    puts("Virtual Machine ready.\n");
     return vm;
 }
 
-void push(VM* vm, Object* obj){
-    assert(vm->stackSize < STACK_MAX, "Stack overflow!");
-    vm->stack[vm->stackSize++] = value;
+Object* createObject(VM* vm, ObjectType objType){
+
+    if(vm->numObject == vm->maxObject) gc(vm);
+
+    Object* obj = malloc(sizeof(Object));
+
+    obj->next = vm->firstObject;
+    vm->firstObject = obj;
+
+    obj->marked = 0;
+    obj->type = objType;
+
+    vm->numObject++;
+    return obj;
 }
 
 Object* pop(VM* vm){
-    assert(vm->stackSize > 0, "Stack underflow!");
-    vm->stack[--vm->stackSize];
-}
-
-Object* createObject(ObjectType objType){
-    Object* obj = malloc(sizeof(Object));
-    obj->marked = 0;
-    obj->type = objType;
+    assert(vm->stackSize > 0);
+    Object* obj = vm->stack[--vm->stackSize];
+    puts("Object poped.");
     return obj;
 }
 
 void pushInt(VM* vm, int value){
-    Object* obj = createObject(INT);
+    Object* obj = createObject(vm, INT);
     obj->intValue = value;
     push(vm, obj);
+
+    printf("Object pushed: %d\n", value);
+    printf("Number of objects: %d\n", vm->numObject);
 }
 
 void pushDouble(VM* vm, double value){
-    Object* obj = createObject(DOUBLE);
+    Object* obj = createObject(vm, DOUBLE);
     obj->intValue = value;
     push(vm, obj);
+
+    printf("Object pushed: %f\n", value);
+    printf("Number of objects: %d\n", vm->numObject);
 }
 
 void pushPair(VM* vm){
-    Object* obj = createObject(PAIR);
+    Object* obj = createObject(vm, PAIR);
     obj->tail = pop(vm);
     obj->head = pop(vm);
-
     push(vm,obj);
+
+    printf("Object pushed:");
+    printObject(obj);
+    puts("");
+    printf("Number of objects: %d\n", vm->numObject);
+}
+
+void push(VM* vm, Object* obj){
+    assert(vm->stackSize < STACK_MAX);
+    vm->stack[vm->stackSize++] = obj;
 }
 
 void markAll(VM* vm){
-
     for(size_t i=0; i<vm->stackSize; i++){
-        mark(vm, stack[i])
+        mark(vm->stack[i]);
     }
 }
 
 void mark(Object* obj){
-
     if(obj->marked) return; // for no loop
 
     obj->marked = 1;
@@ -97,3 +151,81 @@ void mark(Object* obj){
     }
 }
 
+void sweep(VM* vm) {
+    Object** obj = &vm->firstObject;
+
+    while (*obj != NULL) {
+        if ((*obj)->marked == 0) {
+            Object* unreached = *obj;
+            *obj = (*obj)->next;  // aggiorna la lista
+
+            free(unreached);
+            vm->numObject--;
+        } else {
+            (*obj)->marked = 0;
+            obj = &(*obj)->next;
+        }
+    }
+}
+
+void printObject(Object* obj){
+
+    if(obj->type == INT){
+        printf("[%d]->", obj->intValue);
+        return;
+    }
+    
+    if(obj->type == DOUBLE){
+        printf("[%f]->", obj->doubleValue);
+        return;
+    }
+
+    printObject(obj->head);
+    printObject(obj->tail);
+}
+
+void printStack(VM* vm){
+    for(size_t i=0; i<vm->stackSize; i++){
+        printObject(vm->stack[i]);
+    }
+}
+
+void printObjects(VM* vm){
+
+    Object* obj = vm->firstObject;
+    while(obj != NULL){
+        printObject(obj);
+        obj = obj->next;
+    }
+}
+
+void gc(VM* vm) {
+    puts("Garbage collector activated.");
+    printStack(vm);
+    puts("");
+    printObjects(vm);
+    puts("");
+
+    markAll(vm);
+    sweep(vm);
+
+    printStack(vm);
+    puts("");
+    printObjects(vm);
+    puts("");
+}
+
+void main(){
+    
+    printf("Activating threshold: %d\n", INITIAL_THRESHOLD);
+    
+    VM* vm =newVM();
+
+    pushInt(vm, 10);
+    pushInt(vm, 3);
+    pushInt(vm, 1);
+    pushInt(vm, 2);
+    pop(vm);
+    pushInt(vm, 12);
+    pushInt(vm, 13);
+}
