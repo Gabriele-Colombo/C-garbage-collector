864203dbb0ce090445904eed27a237aad18e149e
Gabriele Colombo <colombogabriele0303@gmail.com>
2025-08-15 06:48:19 +0200

    Printing and refinement

--- gc.c
+++ gc.c
@@ -40,7 +40,7 @@ typedef struct fObject{
 // Create a VM as an array of Object
 typedef struct{
 
-    int numObject; // total allocated Object
+    int numObject; // Total allocated Object from last gc
     int maxObject; // Max number of object to activate the gc
 
     Object* firstObject; // Objects created
@@ -61,16 +61,19 @@ void pushPair(VM* vm);
 void mark(Object* obj);
 void sweep(VM* vm);
 void printStack(VM* vm);
-void printObjects(VM* vm);
-void printObject(Object* obj);
+void printAllObjects(VM* vm);
+void printObject(Object* obj, bool address);
 
 
 VM* newVM(){
     VM* vm = malloc(sizeof(VM));
-    vm->firstObject = NULL;
-    vm->stackSize = 0;
+
     vm->numObject = 0;
     vm->maxObject = INITIAL_THRESHOLD;
+
+    vm->firstObject = NULL;
+
+    vm->stackSize = 0;
 
     puts("Virtual Machine ready.\n");
     return vm;
@@ -81,40 +84,30 @@ Object* createObject(VM* vm, ObjectType objType){
     if(vm->numObject == vm->maxObject) gc(vm);
 
     Object* obj = malloc(sizeof(Object));
-
-    obj->next = vm->firstObject;
-    vm->firstObject = obj;
-
     obj->marked = 0;
     obj->type = objType;
+    obj->next = vm->firstObject;
 
+    vm->firstObject = obj;
     vm->numObject++;
     return obj;
 }
 
 Object* pop(VM* vm){
     assert(vm->stackSize > 0);
-    Object* obj = vm->stack[--vm->stackSize];
-    puts("Object poped.");
-    return obj;
+    return vm->stack[--vm->stackSize];
 }
 
 void pushInt(VM* vm, int value){
     Object* obj = createObject(vm, INT);
     obj->intValue = value;
     push(vm, obj);
-
-    printf("Object pushed: %d\n", value);
-    printf("Number of objects: %d\n", vm->numObject);
 }
 
 void pushDouble(VM* vm, double value){
     Object* obj = createObject(vm, DOUBLE);
-    obj->intValue = value;
+    obj->doubleValue = value;
     push(vm, obj);
-
-    printf("Object pushed: %f\n", value);
-    printf("Number of objects: %d\n", vm->numObject);
 }
 
 void pushPair(VM* vm){
@@ -122,11 +115,6 @@ void pushPair(VM* vm){
     obj->tail = pop(vm);
     obj->head = pop(vm);
     push(vm,obj);
-
-    printf("Object pushed:");
-    printObject(obj);
-    puts("");
-    printf("Number of objects: %d\n", vm->numObject);
 }
 
 void push(VM* vm, Object* obj){
@@ -168,64 +156,91 @@ void sweep(VM* vm) {
     }
 }
 
-void printObject(Object* obj){
+void printObject(Object* obj, bool address){
 
-    if(obj->type == INT){
-        printf("[%d]->", obj->intValue);
-        return;
-    }
-    
-    if(obj->type == DOUBLE){
-        printf("[%f]->", obj->doubleValue);
-        return;
-    }
+    switch(obj->type){
 
-    printObject(obj->head);
-    printObject(obj->tail);
+        case INT:
+            if(address){
+                printf("I-[%d](%d)", obj->intValue, obj);
+            }else{
+                printf("I-[%d]", obj->intValue);
+            }
+            break;
+
+        case DOUBLE:
+            if(address){
+                printf("D-[%.2f](%d)", obj->doubleValue, obj);
+            }else{
+                printf("D-[%.2f]", obj->doubleValue);
+            }
+            break;
+        
+        case PAIR:
+            printf("P-[%d][%d]", obj->head, obj->tail);
+    }
 }
 
 void printStack(VM* vm){
+    puts("Stack status:");
+
     for(size_t i=0; i<vm->stackSize; i++){
-        printObject(vm->stack[i]);
+
+        printObject(vm->stack[i], false);
+        if(i != (vm->stackSize)-1) printf(" -> ");
     }
+    printf("\n\n");
 }
 
-void printObjects(VM* vm){
+void printAllObjects(VM* vm){
+    puts("Objects status:");
 
     Object* obj = vm->firstObject;
     while(obj != NULL){
-        printObject(obj);
+        printObject(obj, true);
+        printf("\n");
         obj = obj->next;
     }
+    puts("");
 }
 
 void gc(VM* vm) {
     puts("Garbage collector activated.");
+
+    puts("Before:");
     printStack(vm);
-    puts("");
-    printObjects(vm);
-    puts("");
+    printAllObjects(vm);
 
     markAll(vm);
     sweep(vm);
+    vm->numObject = 0;
 
+    puts("After:");
     printStack(vm);
-    puts("");
-    printObjects(vm);
-    puts("");
+    printAllObjects(vm);
 }
 
 void main(){
     
     printf("Activating threshold: %d\n", INITIAL_THRESHOLD);
     
-    VM* vm =newVM();
+    VM* vm = newVM();
 
     pushInt(vm, 10);
-    pushInt(vm, 3);
+    pushDouble(vm, 3.23);
     pushInt(vm, 1);
-    pushInt(vm, 2);
+    pushDouble(vm, 2.24);
     pop(vm);
-    pushInt(vm, 12);
+    pushPair(vm);
     pushInt(vm, 13);
+    pushInt(vm, 24);
+    pushDouble(vm, 34.1);
+    pushPair(vm);
+    pop(vm);
+    pushInt(vm, 2);
+    pushInt(vm, 6);
+
+    puts("Final:");
+    printStack(vm);
+    printAllObjects(vm);
 }
