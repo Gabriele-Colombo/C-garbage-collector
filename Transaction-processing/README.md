a145cabff5bd8fd7706bf9f4f24c0ec47e41c2e3
Gabriele Colombo <colombogabriele0303@gmail.com>
2025-08-15 13:12:20 +0200

    Started transaction processing

---
+++ Transaction-processing/README.md
@@ -0,0 +1,7 @@
+Implementation of a transaction-processing program using random access files.
+
+The program maintains simple bank's account information and is capable of:
+- updating existing accounts
+- adding new accounts
+- deleting accounts
+- storing a listing of all the courrent accounts in a text file for printing
---
+++ Transaction-processing/Transaction-processing.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+
+#define MAX_ACC 100
+
+typedef struct clientData{
+    int id;
+    char name[15];
+    char surname[20];
+    double balance;
+}ClientData;
+
+//prototypes
+void inizitializeFile(FILE* ptrData);
+void printSelection();
+void createAccount(FILE* ptrData);
+void deleteAccount(FILE* ptrData);
+void updateAccount(FILE* ptrData);
+void ListAccounts(FILE* ptrData);
+
+void main(){
+
+    void (*f[4])(FILE* ptrF) = {createAccount, deleteAccount, updateAccount, ListAccounts};
+
+    FILE* ptrF = fopen("Data.dat", "wb+");
+
+    if(ptrF == NULL){
+
+        puts("Unable to open file .dat");
+        
+    }else{
+
+        inizitializeFile(ptrF);
+
+        while(true){
+
+            int account = 0;
+            printSelection();
+            scanf("%d", &account);
+
+            while(account<1 || account>5){
+
+                printf("Select a correct option.\n");
+                printSelection();
+                scanf("%d", &account);
+            }
+
+            if(account==5) break;
+            (*f[account-1])(ptrF);
+        }
+    }
+
+    fclose(ptrF);
+}
+
+void inizitializeFile(FILE* ptrData){
+
+    ClientData client = {0, "", "", 0.0};
+
+    for(size_t i=0; i<MAX_ACC; i++){
+        fwrite(&client, sizeof(ClientData), 1, ptrData);
+    }
+
+    rewind(ptrData);
+}
+
+
+void printSelection(){
+    printf("Select an option:\n"
+            "1 - Create a new account.\n"
+            "2 - Delete an account.\n"
+            "3 - Update an account.\n"
+            "4 - List all accounts.\n"
+            "5 - Exit\n"
+        );
+}
+
+void createAccount(FILE* ptrData){
+    
+    int account = 0;
+    printf("Select account number (1-100): ");
+    scanf("%d", &account);
+
+    while(account<1 || account>MAX_ACC){
+
+        printf("Select a correct number.\n");
+        printf("Select account number (1-100): ");
+        scanf("%d", &account);
+    }
+
+    ClientData client = {0, "", "", 0.0};
+
+    fseek(ptrData, (account-1)*sizeof(ClientData), SEEK_SET);
+
+    int numR = fread(&client, sizeof(ClientData), 1, ptrData);
+    if(numR == 0){
+        puts("File could not be read.");
+        return;
+    }
+
+    if(client.id != 0){
+        puts("Selected account id already used.");
+    }else{
+
+        client.id = account;
+        client.balance = 0;
+
+        printf("Client name: ");
+        scanf("%15s", client.name);
+
+        printf("Client surname: ");
+        scanf("%15s", client.surname);
+        
+        fseek(ptrData, (client.id-1)*sizeof(ClientData), SEEK_SET);
+        fwrite(&client, sizeof(ClientData), 1, ptrData);
+    }
+}
+
+void deleteAccount(FILE* ptrData){
+    printf("Elimino account");
+}
+
+void updateAccount(FILE* ptrData){
+    printf("Aggiorno account");
+}
+
+void ListAccounts(FILE* ptrData){
+    
+    FILE* listF = fopen("List.txt", "w");
+
+    if(listF == NULL){
+        puts("Unable to open file .txt");
+        return;
+    }
+
+    fprintf(listF, "%-10s\t%-17s\t%-22s\t%s\n", "Account", "Name", "Surname", "Balance");
+
+    rewind(ptrData);
+
+    ClientData client = {0, "", "", 0.0};
+
+    while(!feof(ptrData)){
+
+        int result = fread(&client, sizeof(ClientData), 1, ptrData);
+        if(client.id != 0){
+            fprintf(listF, "%-10d\t%-17s\t%-22s\t%.3f\n", client.id, client.name, client.surname, client.balance);
+        }
+    }
+
+    fclose(listF);
+}
